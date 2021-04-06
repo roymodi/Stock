664802a0c760e98c0ebaa267466d26895bd2a4be
roymodi <roymodi63@gmail.com>
2021-04-06 19:50:40 +0530

    daravatives index add

--- YFindia.py
+++ YFindia.py
@@ -11,6 +11,8 @@ class YahooIndia:
     def __init__(self):
         self.purl = "https://in.finance.yahoo.com/quote/{0}?p={0}"
         self.history_url = "https://query1.finance.yahoo.com/v7/finance/download/{0}?period1={dayfrom}&period2={timenow}&interval=1d&events=history"
+        self.bank_ = "https://query1.finance.yahoo.com/v7/finance/download/^NSEBANK?period1={dayfrom}&period2={timenow}&interval=1d&events=history&includeAdjustedClose=true"
+        self.nifty_ = "https://query1.finance.yahoo.com/v7/finance/download/^NSEI?period1={dayfrom}&period2={timenow}&interval=1d&events=history&includeAdjustedClose=true"
                            
     def web(self, url):
         ws= Webscraping()
@@ -40,17 +42,38 @@ class YahooIndia:
         info = dict(zip(key, value))
         return info
 
-    def historydata(self, stock, dd_mm_yy):
-        stock = (stock.upper()) + ".NS"
+    def timenow(self):
         now = datetime.utcnow()
-        timenow = str(int(now.timestamp()))
+        tnow = str(int(now.timestamp()))
+        return tnow
+    
+    def dayfrom(self,dd_mm_yy):
         date = (((dd_mm_yy.replace(",", " ")).replace("-", " ")).replace("/", " ")).split(" ")
         dd = int(date[0])
         mm = int(date[1])
         yy = int(date[2])
         hd_datetime = datetime(yy, mm, dd)
+        now = datetime.utcnow()
         dt = now - hd_datetime
-        dayfrom = str(int((now - dt).timestamp()))
-        file = self.web(self.history_url.format(stock, dayfrom=dayfrom, timenow=timenow))
-        dataframe = pd.read_csv(StringIO(file.text), parse_dates=["Date"])
+        dfrom = str(int((now - dt).timestamp()))
+        return dfrom
+
+    def historydata(self, stock, dd_mm_yy):
+        stock = (stock.upper()) + ".NS"
+        file_ = self.web(self.history_url.format(stock, dayfrom=self.dayfrom(dd_mm_yy), timenow=self.timenow()))
+        dataframe = pd.read_csv(StringIO(file_.text), parse_dates=["Date"])
         return dataframe
+    
+    def banknifty(self,dd_mm_yy):
+        file_ = self.web(self.bank_.format(dayfrom= self.dayfrom(dd_mm_yy), timenow= self.timenow()))
+        dataframe = pd.read_csv(StringIO(file_.text), parse_dates=["Date"])
+        return dataframe
+    
+    def nifty(self,dd_mm_yy):
+        file_ = self.web(self.nifty_.format(dayfrom= self.dayfrom(dd_mm_yy), timenow= self.timenow()))
+        dataframe = pd.read_csv(StringIO(file_.text), parse_dates=["Date"])
+        return dataframe
+        
+
+
+    
