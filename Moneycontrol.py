143384ca72fe667f5a576f6fc9bc7a9691833296
kkmodi <kkmodi63@gmail.com>
2021-06-17 19:45:12 +0530

    modifi

--- Moneycontrol.py
+++ Moneycontrol.py
@@ -1,23 +1,33 @@
-
 from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
 import os,re
 import json
 import pickle
 import requests
-import numpy
-from lxml import html
+import re
 
 class Moneycontrol:
     def __init__(self):
-        self.stocklisturl = "https://www.moneycontrol.com/india/stockpricequote/"
+        self.sug_url = "https://www.moneycontrol.com/mccode/common/autosuggestion_solr.php?classic=true&query={}&type=1&format=json&callback=suggest1"
         self.latterurl = "https://www.moneycontrol.com/india/stockpricequote/{}"
-        self.search = "https://www.moneycontrol.com/stocks/cptmarket/compsearchnew.php?search_data=&cid=&mbsearch_str=&topsearch_type=1&search_str={}"
+
+    def create_path(self):
+        folder_name = 'Temp_file'  # this is folder name
+        current_dir = os.getcwd()  # this is path
+        path = os.path.join(current_dir, folder_name)  # create folder path
+        try:
+            os.mkdir(path)
+        except FileExistsError:
+            pass
+        return path
+    
     def useragent(self):
         ua = UserAgent()
         hd = ua.ie
         header = {
             'Host': 'www.moneycontrol.com',
+            'Origin':'https://www.moneycontrol.com',
+            'Referer':'https://www.moneycontrol.com/',
             'User-Agent': hd,
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
             'Accept-Language': 'en-GB,en;q=0.5',
@@ -27,18 +37,11 @@ class Moneycontrol:
             'Cache-Control': 'max-age=0'
             }
         return header
-
+    
     def cokie(self):
-        folder_name ='Temp_file' # this is folder name
-        get_path = os.getcwd() # this is path where open programm
-        create_path = os.path.join(get_path,folder_name) # create folder path
-        try:
-            os.mkdir(create_path)
-        except FileExistsError:
-            pass
         h = self.useragent()
-        flist = os.listdir(create_path) # file list
-        file = create_path+'\\moneycontrol' # create file path with name
+        flist = os.listdir(self.create_path()) # file list
+        file = self.create_path()+'\\moneycontrol' # create file path with name
         if 'moneycontrol' in flist:
             with open(file, 'rb')as op:
                 cokie = pickle.load(op)
@@ -48,13 +51,15 @@ class Moneycontrol:
             with open(file, 'wb')as op:
                 pickle.dump(cokie, op)
         return cokie
-
-    def web(self, url):
-        get_path = os.getcwd()
-        create_path = os.path.join(get_path,'Temp_file')
-        file = create_path+'\\moneycontrol'
+    
+    def web(self,url,host='None'):
+        file = self.create_path()+'\\moneycontrol'
         c = self.cokie()
-        h = self.useragent()
+        if host == 'None':
+            h = self.useragent()
+        else:
+            h = self.useragent()
+            h['Host'] = 'priceapi.moneycontrol.com'
         while True:
             try:
                 page1 = requests.get(url, cookies=c, headers=h, timeout=5)
@@ -69,202 +74,170 @@ class Moneycontrol:
             except:
                 pass
         return page
+    
+    def find_id(self,link):
+        def filter_id_(id_):
+            sh = re.search('amp;',id_)
+            if sh != None:
+                idd = id_.replace('amp;','')
+            else:
+                idd = id_
+            return idd
+        try:
+            page = self.web(link)
+            tag = re.search('inditrade', page.text)
+            span1 = (tag.span())[0]
+            span2 = ((tag.span())[1])+97
+            line = (page.text)[span1:span2]
+            nseid = filter_id_((line.split("'"))[1])
+            mcid = (line.split("'"))[3]
+            return nseid,mcid,page
+        except:
+            pass
+    
+    def suggestion(self,ticket):
+        page = self.web(self.sug_url.format((ticket.upper())))
+        tup_page = (eval((page.text).replace('suggest1','')))
+        temp = {}
+        for x in tup_page:
+            dt = dict(x)
+            if 'No Result Available' != dt['pdt_dis_nm']:
+                try:
+                    link = (dt['link_src']).replace('\t','')
+                    nseid = (((dt['pdt_dis_nm']).split('\t'))[2]).replace(', ','')
+                    temp[nseid] = link
+                except:
+                    pass
+            else:
+                pass
+        return temp
+
+    def mc_sug_id(self,symbole):
+        ticket = symbole.upper()
+        dt = self.suggestion(ticket)
+        try:
+            link = dt[ticket] if (not dt)==False else None # chake dict(dt) are empty or not
+            if link != None:
+                mctup = self.find_id(link)
+                if ticket == mctup[0]:
+                    page = mctup[2]
+                    mcid = mctup[1]
+                else:
+                    pass
+                return mcid,page
+        except:
+            pass
+        
+    
+    
+    
+    def matchWord(self,symbole,stockname):
+        ticket = symbole.upper()
+        def char_list(ticket):
+            num_list = []
+            for x in ((ticket.replace(' ','')).upper()):
+                num_list.append(ord(x))
+            return num_list
+
+        tik = char_list(ticket)
+        stk = char_list(stockname)
+        tmp = 0
+        c = 0
+        for x in tik:
+            try:
+                if x==stk[c]:
+                    tmp += 1
+            except:
+                pass
+            c+=1
+        
+        st_len = len(stk)
+
+        m_value = abs(((tmp*100)/st_len))
+
+        return m_value
+    
+    def matchDict(self,dt,symbole):
+        ticket = symbole.upper()
+        temp = {}
+        for x in dt:
+            if x == '':
+                pass
+            else:
+                temp[x] = self.matchWord(ticket,x)
+        def sort_Dict(dt):
+            temp_dt = {}
+            for x in sorted(dt.values()):
+                for y in dt:
+                    if dt[y]==x:
+                        temp_dt[y] = dt[y]
+            return temp_dt
+        rt = sort_Dict(temp)
+        return rt
+    
     def findLatter(self,ticket):
         tk = ticket.upper()
         latter = tk[0]
         return latter
     
-    def symbole(self,string):
-        try:
-            st = str(string).split(' ')
-            for x in st:
-                if x.startswith(':'):
-                    snam = x.replace(':','')
-                    if snam.isalpha():
-                        name = snam
-                    elif re.search(r'-',snam):
-                        name = snam
-            return name
-        except:
-            pass
-
-    def linkDict(self,url):
-        page = self.web(url)
-        soup = BeautifulSoup(page.text,'html.parser')
-        linkdt = {}
-        for x in soup.find_all('td'):
-            tagA = str(x.find('a'))
-            if tagA != 'None':
-                linksoup = BeautifulSoup(tagA,'html.parser')
-                for y in linksoup:
-                    ylink = y.get('href')
-                    ytitle = (((y.text).replace('\t','')).replace('\n',''))
-                    linkdt[ytitle]=ylink
-            else:
-                pass
+    def linkDict(self,latter): # latter argument class delete
+        file_list = os.listdir(self.create_path())
+        fileName = self.create_path()+'\\'+latter+'.json'
+        if latter +'.json' in file_list:
+            with open(fileName, 'r')as op:
+                linkdt = json.load(op)
+        else:
+            page = self.web(self.latterurl.format(latter))
+            soup = BeautifulSoup(page.text,'html.parser')
+            linkdt = {}
+            for x in soup.find_all('td'):
+                tagA = str(x.find('a'))
+                if tagA != 'None':
+                    linksoup = BeautifulSoup(tagA,'html.parser')
+                    for y in linksoup:
+                        ylink = y.get('href')
+                        ytitle = (((y.text).replace('\t','')).replace('\n',''))
+                        linkdt[ytitle]=ylink
+                else:
+                    pass
+            with open(fileName, 'w')as op:
+                json.dump(linkdt, op)
         return linkdt
-
-    def searchDict(self,url):
-        page = self.web(url)
-        soup = BeautifulSoup(page.text,'html.parser')
-        linkdic = {}
-        for x in soup.find_all('td'):
-            if 'class="bl_18"'in str(x):
-                tagA=x.find('a')
-                link = tagA.get('href')
-                name = tagA.text
-                linkdic[name]=link
-            else:
-                if 'NSE Id' in str(x):
-                    sym = self.symbole(x.text)
-                    linkdic[sym]=linkdic.pop(name)
-                else:
-                    pass
-        return linkdic
     
-    def matchWord(self,ticket,stockname):
-        ticket_list = []
-        for x in ((ticket.replace(' ','')).upper()):
-            ticket_list.append(ord(x))
-        stockname_list = []
-        for y in ((stockname.replace(' ','')).upper()):
-            stockname_list.append(ord(y))
-        matchlist = [x for x in ticket_list if x in stockname_list]
-        result = all(map(lambda x, y: x == y, ticket_list, matchlist))
-        if len(ticket_list)<len(stockname_list):
-            if result :
-                median_value = abs((numpy.median(ticket_list))-(numpy.median(stockname_list)))
-                mean_value = abs((numpy.mean(ticket_list))-(numpy.mean(stockname_list)))
-                std_value = abs((numpy.std(ticket_list))-(numpy.std(stockname_list)))
-                if (mean_value<1)and(median_value<1)and(std_value<1):
-                    rt = ticket
-                else:
-                    rt = None
-            else:
-                rt = None
-        else:
-            if result:
-                notmatch = len([x for x in stockname_list if x not in ticket_list])
-                std_value = abs((numpy.std(ticket_list))-(numpy.std(stockname_list)))
-                if std_value<1 and notmatch==1 and abs(len(ticket)-len(stockname))==1:
-                    rt = ticket
-                else:
-                    rt = None
-            else:
-                rt = None
-        return rt
-
-    def matchDic(self,dt,ticket):
-        mdict ={}
-        for x in dt:
-            if x == '':
+    def mc_latter_id(self,dt,ticket):
+        symbole = ticket.upper()
+        keys = reversed(list(dt))
+        for x in keys:
+            print(x)
+            link = (self.linkDict(self.findLatter(symbole)))[x]
+            st_id = self.find_id(link)
+            if st_id == None:
                 pass
+            elif x == symbole:
+                return st_id[1],st_id[2]
+                break
+            elif st_id[0] == symbole:
+                return st_id[1],st_id[2]
+                break
             else:
-                key = self.matchWord(ticket,(x.upper()))
-                if key==ticket:
-                    value = dt[x]
-                    mdict[key]=value
-                else:
-                    pass
-        return mdict
+                pass
     
-    def upperDict(self,dt):
-        try:
-            udict ={}
-            for x in dt:
-                key = x.upper()
-                value = dt[x]
-                udict[key]=value
-            return udict
-        except:
-            pass
-##########################################
-    def create_path(self):
-        folder_name = 'Temp_file'  # this is folder name
-        current_dir = os.getcwd()  # this is path
-        path = os.path.join(current_dir, folder_name)  # create folder path
-        try:
-            os.mkdir(path)
-        except FileExistsError:
-            pass
-        return path
-
-    def creatname(self,url):
-        nstr = (url.replace('https://','')).split('.')
-        try:
-            str1 = nstr[1]
-            # remove any spacial charater 
-            sr = (''.join(x for x in str1 if x.isalpha()))
-        except IndexError:
-            str1 = nstr[0]
-            sr = (''.join(x for x in str1 if x.isalpha()))
-        except :
-            str1 = "Defult"
-            sr = (''.join(x for x in str1 if x.isalpha()))
-        return sr     
-#####################################################
-    def findLink(self,ticket):
-        # tk = (((ticket.replace(' ','')).replace('.','')).replace('-','')).upper()
-        tk = ticket.upper()
-        try:
-            defaultDict = self.upperDict(self.searchDict(self.search.format(ticket)))
-            if tk in defaultDict:
-                link = defaultDict[tk]
-            else:
-                latter = self.findLatter(ticket)
-                latterDict = self.matchDic(self.linkDict(self.latterurl.format(latter)),ticket)
-                if tk in latterDict:
-                    link = latterDict[tk]
-                else:
-                    otherDict = self.upperDict(self.linkDict(self.latterurl.format('others')))
-                    if tk in otherDict:
-                        link = otherDict[tk]
-                    else:
-                        chakelist = self.upperDict(self.linkDict(self.stocklisturl))
-                        if tk in chakelist:
-                            link = chakelist[tk]
-                        else:
-                            link = None
-        except:
-            link = None
-        return link
-       
-    
-
-    def result (self,ticket):
-        link = self.findLink(ticket)
-        if link != None:
-            page = self.web(link)
+    def find_page(self,symbole):
+        ticket = symbole.upper()
+        sug = self.mc_sug_id(ticket)
+        if sug != None:
+            mc_id = sug[0]
+            page = sug[1]
+            return mc_id,page
         else:
-            page = self.web(self.search.format(ticket))
-        return page
+            latter = self.findLatter(ticket)
+            latterDict = self.linkDict(latter)
+            m_dict = self.matchDict(latterDict,ticket)
+            if (not m_dict) != True:
+                m_id = ((self.mc_latter_id(m_dict,ticket))[0])
+                page = (self.mc_latter_id(m_dict,ticket))[1]
+                return m_id,page
+            else:
+                pass
 
 
-    
-    def livePrice(self,ticket):
-        page = self.result(ticket)
-        soup = BeautifulSoup(page.text,'html.parser')
-        price = soup.find(class_='inprice1 nsecp')
-        try:
-            return price.get_text()
-        except:
-            return price
-
-# mc = Moneycontrol()
-# print(mc.livePrice('BHARTIARTL'))
-        
-
-# import Nse
-# nse = Nse.NseData()
-# df = nse.nifty_50()
-# co_name = df['SYMBOL'].iloc[2:50]
-# # print(co_name)
-# mc = Moneycontrol()
-# for x in co_name:
-#     print(x)
-#     print(x,' = ',mc.livePrice(x))
-
-
-
-# mc = Moneycontrol()
-# print(mc.result('ABAN'))
